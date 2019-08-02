b79a4d346e273521eabc445102504e99c4f1b90d
O. Mochalov <omochalov15@gmail.com>
2019-08-02 13:31:22 +0200

    modify userService

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,10 +1,13 @@
 const dbQuery = require('../database/dbQuery');
 
-const create = token => dbQuery.executeQuery('INSERT INTO users VALUES (DEFAULT, $1::text)', [token]);
-
 const findByToken = async (token) => {
   const result = await dbQuery.executeQuery('SELECT * FROM users WHERE application_token = $1::text', [token]);
   return result.rows[0] ? result.rows[0] : null;
+};
+
+const create = async (token) => {
+  await dbQuery.executeQuery('INSERT INTO users VALUES (DEFAULT, $1::text)', [token]);
+  return findByToken(token);
 };
 
 module.exports = {
