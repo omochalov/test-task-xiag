1a584aa6be2d601f6506e0473c03a3f3b679a47f
O. Mochalov <omochalov15@gmail.com>
2019-08-02 20:56:10 +0200

    fix needContinue

--- src/utils/responder.js
+++ src/utils/responder.js
@@ -3,22 +3,24 @@ const CONTENT_TYPES = require('../consts/contentTypes');
 
 const baseJsonResponse = (res, obj) => {
   if (typeof obj === 'string') return res.end(obj);
-  res.needContinue = false;
   return res.end(JSON.stringify(obj));
 };
 
 const notFoundResponse = (res, url) => {
   res.writeHead(STATUS_CODES.NOT_FOUND, CONTENT_TYPES.JSON);
+  res.needContinue = false;
   return baseJsonResponse(res, { error: `Not found: ${url.pathname}` });
 };
 
 const badResponse = (res, obj) => {
   res.writeHead(STATUS_CODES.BAD, CONTENT_TYPES.JSON);
+  res.needContinue = false;
   return baseJsonResponse(res, obj);
 };
 
 const okResponse = (res, obj) => {
   res.writeHead(STATUS_CODES.OK, CONTENT_TYPES.JSON);
+  res.needContinue = true;
   return baseJsonResponse(res, obj);
 };
 
